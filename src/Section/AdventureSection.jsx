0d3398e95007ee919a742993619ad972aee933b9
NoobMaester55 <133396708+NoobMaester55@users.noreply.github.com>
2024-07-24 07:30:31 +0545

    Update AdventureSection.jsx

--- src/Section/AdventureSection.jsx
+++ src/Section/AdventureSection.jsx
@@ -1,11 +1,7 @@
-import React, { useEffect, Suspense, lazy } from 'react';
-
-// PART - 1
-const Hero = lazy(() => import('../Component/BackgroundWithText/BackgroundWithText'));
+import React, { useEffect } from 'react';
+import Hero from '../Component/BackgroundWithText/BackgroundWithText';
 import Adventure from '/image/Zipline.jpg';
-
-// PART - 2
-const Places = lazy(() => import('../Component/HorizontalCard/HorizontalCard'));
+import Places from '../Component/HorizontalCard/HorizontalCard';
 import { data } from '../Data/data';
 
 function AdventureSection() {
@@ -13,30 +9,27 @@ function AdventureSection() {
     window.scrollTo(0, 0);
   }, []);
 
-  // PART - 1
   const texts = ["Discover Nepal's Thrilling Adventures", "Embark on Epic Journeys", "Experience the Ultimate Adventure", "Explore Nepal's Adventure Wonderland"];
   const filteredData = data.filter(item => item.categories.includes('adventure'));
 
   return (
     <>
-      
-        {/* PART - 1 */}
-        <Hero texts={texts} background={Adventure} />
-      
-      
-        {/* PART - 2 */}
-        {filteredData.map((item, index) => (
-          <Places
-            key={index}
-            Imgsrc={item.Imgsrc}
-            Title={item.Title}
-            Description={item.Description}
-            learnMorePath={item.learnMorePath}
-            reverse={index % 2 !== 0}
-          />
-        ))}
-      
+      {/* PART - 1 */}
+      <Hero texts={texts} background={Adventure} />
+
+      {/* PART - 2 */}
+      {filteredData.map((item, index) => (
+        <Places
+          key={index}
+          Imgsrc={item.Imgsrc}
+          Title={item.Title}
+          Description={item.Description}
+          learnMorePath={item.learnMorePath}
+          reverse={index % 2 !== 0}
+        />
+      ))}
     </>
   );
 }
+
 export default AdventureSection;
